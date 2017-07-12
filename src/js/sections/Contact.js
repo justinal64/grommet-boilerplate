9d020739ce4a2bbe0a01019118910212d11d67b1
justin leggett <justin@codingrookie.com>
2017-07-12 11:45:02 -0500

    setup AboutMe, Contact, Project sections and added custom.scss

--- src/js/sections/Contact.js
+++ src/js/sections/Contact.js
@@ -3,20 +3,20 @@ import React, { Component } from 'react';
 import Headline from 'grommet/components/Headline';
 import Section from 'grommet/components/Section';
 
-class Intro extends Component {
+class Contact extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <div className='intro-bg'>
+      <div className='contact-bg'>
         <Section pad='large' justify='center' align='center' full='vertical'>
-          <Headline margin='none'>Intro</Headline>
+          <Headline margin='none'>Contact</Headline>
         </Section>
       </div>
     );
   }
 }
 
-export default Intro;
+export default Contact;
